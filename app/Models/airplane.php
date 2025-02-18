3a1aeba1048c32acfc9e7a5e5c1362ea7ba7d453
issamchlf <issamchellaf734@gmail.com>
2025-02-18 10:56:15 +0100

    Feat : add HasFactory trait to Airplane and Flight models

--- app/Models/airplane.php
+++ app/Models/airplane.php
@@ -3,9 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Airplane extends Model
 {
+    use HasFactory;
     protected $table = 'airplanes';
     protected $fillable = ['name', 'type', 'max_seats'];
     
